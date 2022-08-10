120f2eba1b20b3baf47f406392316dd6d85d6c23
ryotaro <ryo.ex3991@gmail.com>
2022-08-10 16:24:35 +0900

    ちょっと整理

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ pub mod nes;
 
 fn main() {
     let rom = "rom/hello.nes";
+    // let rom = "rom/nestest.nes";
     nes::run(rom);
 }
--- src/nes.rs
+++ src/nes.rs
@@ -9,7 +9,6 @@ pub mod optable;
 
 extern crate sdl2;
 
-use sdl2::*;
 use crate::nes::cpu::*;
 use crate::nes::interrupts::*;
 use crate::nes::game::*;
@@ -17,8 +16,6 @@ use crate::nes::ppu::*;
 use crate::nes::render::*;
 use crate::nes::ram::Ram;
 use crate::nes::cassette::Cassette;
-use std::rc::*;
-use std::cell::*;
 
 const WRAM_SIZE: usize = 0x0800;
 const VRAM_SIZE: usize = 0x0800;
@@ -53,17 +50,13 @@ pub fn run(cassette_path: &str) {
     let mut wram: Ram = Ram::new(WRAM_SIZE);
     let mut vram: Ram = Ram::new(VRAM_SIZE);
     let cas: Cassette = Cassette::new(cassette_path);
-    // let mut ctx: Context = Context::new(
-    //     &cas, &mut wram, &mut vram
-    // );
-
     let mut inter: Interrupts = Interrupts::new();;
     let mut ppu: Ppu = Ppu::new(&cas, &mut vram);
     let mut cpu: Cpu = Cpu::new(&cas, &mut wram);
-    cpu.reset(&mut ppu);
-
     let mut game: Game = Game::new().unwrap();
     let mut render: Render = Render::new();
+
+    cpu.reset(&mut ppu);
 
     loop {
         let cycle: u64 = cpu.run(&mut ppu, &mut inter);
